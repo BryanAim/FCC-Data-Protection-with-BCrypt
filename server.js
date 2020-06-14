43e9cfdec62a28fbea469a800b4471da15873ce4
Isale <isale.bryan@gmail.com>
2020-06-14 14:06:47 +0300

    Hash and Compare Passwords Asynchronously

--- server.js
+++ server.js
@@ -13,6 +13,17 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+
+  bcrypt.compare(myPlaintextPassword, hash, (err, result)=> {
+    console.log(result);
+    
+  })
+
+})
+
+
 
 
 
